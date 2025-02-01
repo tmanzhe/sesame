8458391630e0a4a7d834d149cbd467f283aededb
truman <trumanzhe@gmail.com>
2025-02-01 01:18:50 -0500

    added folder structure

---
+++ backend/demo/src/test/java/com/example/demo/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //this test check if the spring context loads succesfully
+    }
+}
